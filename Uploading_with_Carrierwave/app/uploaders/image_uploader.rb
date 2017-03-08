5ae1efdf2c352c3f793a19d0a0d98cffef5e249a
bodrovis <ibodrov@campaigner.ru>
2017-03-08 20:13:48 +0300

    some preparations for new articles

--- Uploading_with_Carrierwave/app/uploaders/image_uploader.rb
+++ Uploading_with_Carrierwave/app/uploaders/image_uploader.rb
@@ -44,6 +44,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def content_type_whitelist
+    /image\//
+  end
+
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
